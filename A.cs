46b308a8ceea0cfe6a3a82bdd3b28082f1b74566
Bogdan Kiptenko <bogdankiptenko@gmail.com>
2020-03-07 12:13:21 +0200

    Добавьте файлы проекта.

---
+++ A.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Task1
+{
+    class A
+    {
+        float a = 3;
+        float b = 5;
+        public float C
+        {
+            get
+            {
+                return a * b;
+            }
+        }
+
+        public A()
+        { }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab4Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            A obj1 = new A();
+            float c = obj1.C;
+            Console.WriteLine("a*b = {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
